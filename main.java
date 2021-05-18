f4487a6494091863482751bc43c96ad47c60c27c
root <root@ip-172-31-18-81.us-east-2.compute.internal>
2021-05-18 09:28:34 +0000

    product-release1-feature1 commit

--- main.java
+++ main.java
@@ -1,5 +1,8 @@
 class HelloWorld {
     public static void main( String []args ) {
-        System.out.println( "Hello World!" );
+         int i=10;
+        System.out.println( "Hello World!",+i );
+        System.out.println("product-release1-feature1")
+        
     }
 }
